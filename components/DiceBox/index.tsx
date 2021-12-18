4327e5b011aa4e77b15961002cf431d0cf9aae89
SPK137 <o.sippakorn@gmail.com>
2021-12-18 13:46:10 +0700

    feat: change physics for dice box

--- components/DiceBox/index.tsx
+++ components/DiceBox/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useRef } from "react";
 import { MeshProps } from "@react-three/fiber";
 import { DoubleSide, Mesh } from "three";
 
-import { Triplet, useBox } from "@react-three/cannon";
+import { Triplet, useBox, usePlane } from "@react-three/cannon";
 
 export interface DiceBoxProps {
   position: Triplet;
@@ -14,63 +14,60 @@ const DiceBox: FC<DiceBoxProps & MeshProps> = ({
   color,
   ...props
 }) => {
-  const ref = useRef<Mesh>();
-
-  const [plane] = useBox(() => ({
-    mass: 0,
-    velocity: [0, 0, 0],
-    position: [0, -4, 0],
+  const [plane] = usePlane(() => ({
+    type: "Static",
+    position: [0, -4.5, 0],
     rotation: [-Math.PI / 2, 0, 0],
-    args: [20, 20, 0.1],
+    args: [20, 20],
   }));
 
-  const [plane2] = useBox(() => ({
-    mass: 0,
+  const [wall1] = useBox(() => ({
+    type: "Static",
     position: [0, -2, 10],
     rotation: [0, 0, 0],
     args: [20, 5, 0.1],
   }));
 
-  const [plane3] = useBox(() => ({
-    mass: 0,
+  const [wall2] = useBox(() => ({
+    type: "Static",
     position: [0, -2, -10],
     rotation: [0, 0, 0],
     args: [20, 5, 0.1],
   }));
 
-  const [plane4] = useBox(() => ({
-    mass: 0,
+  const [wall3] = useBox(() => ({
+    type: "Static",
     position: [10, -2, 0],
     rotation: [0, -Math.PI / 2, 0],
-    args: [20, 5, 0.1],
+    args: [20, 5, 0.5],
   }));
 
-  const [plane5] = useBox(() => ({
-    mass: 0,
+  const [wall4] = useBox(() => ({
+    type: "Static",
     position: [-10, -2, 0],
     rotation: [0, -Math.PI / 2, 0],
-    args: [20, 5, 0.1],
+    args: [20, 5, 0.5],
   }));
 
   return (
     <>
-      <mesh ref={plane} scale={1}>
-        <boxGeometry args={[20, 20, 0.1]} />
+      <mesh ref={plane}>
+        <planeGeometry args={[20, 20]} />
         <meshStandardMaterial color={color} side={DoubleSide} />
       </mesh>
-      <mesh ref={plane2} scale={1}>
+      <mesh ref={wall1}>
         <boxGeometry args={[20, 5, 0.1]} />
         <meshStandardMaterial color={color} side={DoubleSide} />
       </mesh>
-      <mesh ref={plane3} scale={1}>
+      <mesh ref={wall2}>
         <boxGeometry args={[20, 5, 0.1]} />
         <meshStandardMaterial color={color} side={DoubleSide} />
       </mesh>
-      <mesh ref={plane4} scale={1}>
+      <mesh ref={wall3}>
         <boxGeometry args={[20, 5, 0.1]} />
         <meshStandardMaterial color={color} side={DoubleSide} />
       </mesh>
-      <mesh ref={plane5} scale={1}>
+      <mesh ref={wall4}>
         <boxGeometry args={[20, 5, 0.1]} />
         <meshStandardMaterial color={color} side={DoubleSide} />
       </mesh>
